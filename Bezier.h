ceccb1fd9ff2293fbe1c7e8386be391a0836f21a
Paul Rauca <paul.rauca10@gmail.com>
2019-09-25 13:05:37 +0100

    Add smoothness setting for bezier curve.

--- Bezier.h
+++ Bezier.h
@@ -9,6 +9,20 @@ public:
 
 	void draw(SDL_Renderer *renderer) const;
 
+	// Setter and getter for bezier smoothness
+	void setSmoothness(const unsigned int to)
+	{
+		if (to > 0)
+			bezierSmoothness = to;
+	}
+	unsigned int getSmoothness()
+	{
+		return bezierSmoothness;
+	}
+
 private:
 	Vector2 p0, p1, p2, p3;
+
+	// Number of lines the curve should be comprised of
+	static unsigned int bezierSmoothness;
 };
