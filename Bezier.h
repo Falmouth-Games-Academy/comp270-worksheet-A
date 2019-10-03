35237850462110dc29bf1c7bcc2458f7b043dccb
Ashley Sands <ashsands1@amstrike.com>
2019-10-03 10:13:08 +0100

    Compleated all the tasks on the work sheet

--- Bezier.h
+++ Bezier.h
@@ -9,6 +9,14 @@ public:
 
 	void draw(SDL_Renderer *renderer) const;
 
+	Vector2 get_curve_point(float t) const;
+	float get_move_percentage(float current_position, float move_speed);
+
+
 private:
+
 	Vector2 p0, p1, p2, p3;
+
+	float get_distance(Vector2 pos_1, Vector2 pos_2);
+
 };
