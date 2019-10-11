69fb04baa2de75929cbacbf75258d87132fcff81
bc216857 <bc216857@falmouth.ac.uk>
2019-10-11 18:56:34 +0100

    # ADDED * CalculateBezier function that returns a Vector2 to be drawn to screen. * Draw function now draws the wiggly bezier boii.

--- Bezier.cpp
+++ Bezier.cpp
@@ -4,9 +4,23 @@
 Bezier::Bezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3)
 	: p0(p0), p1(p1), p2(p2), p3(p3)
 {
+	for (int i = 0; i < CURVEYSAMPLES + 1; i++)
+	{
+		incrementalPoints[i] = CalculateBezier(float(i) / CURVEYSAMPLES);
+	}
 }
 
 void Bezier::draw(SDL_Renderer *renderer) const
 {
-	// TODO
+	for (int i = 0; i < CURVEYSAMPLES; i++)
+	{
+		SDL_RenderDrawLineF(renderer, incrementalPoints[i].x, incrementalPoints[i].y, incrementalPoints[i + 1].x, incrementalPoints[i + 1].y);
+	}
+}
+
+Vector2 Bezier::CalculateBezier(float t)
+{
+	Vector2 bezierCurvePoints = Vector2();
+	bezierCurvePoints = pow((1.0f - t), 3.0f)*p0 + 3.0f * pow((1.0f - t), 2.0f)*t*p1 + 3.0f * (1.0f - t) * pow(t, 2.0f) * p2 + pow(t, 3.0f) * p3;
+	return bezierCurvePoints;
 }
--- Bezier.h
+++ Bezier.h
@@ -10,5 +10,8 @@ public:
 	void draw(SDL_Renderer *renderer) const;
 
 private:
+	const static int CURVEYSAMPLES = 20;
 	Vector2 p0, p1, p2, p3;
+	Vector2 incrementalPoints[CURVEYSAMPLES + 1];
+	Vector2 CalculateBezier(float t);
 };
