f3ccd8924efe3ab897c9f271949ace9bedcbe104
Ollie <ot218255@falmouth.ac.uk>
2019-10-14 11:30:13 +0100

    Changed the amount of lines within the curve again.

--- Bezier.h
+++ Bezier.h
@@ -11,7 +11,7 @@ public:
 
 private:
 
-	const int static BEZIERSEGMENTS = 15;
+	const int static BEZIERSEGMENTS = 20;
 	Vector2 p0, p1, p2, p3;
 	Vector2 Points[BEZIERSEGMENTS + 1];
 	Vector2 BezierCalculation (float t);
