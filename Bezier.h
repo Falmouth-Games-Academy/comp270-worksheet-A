2aefe3884ef92f8fc24248dfbb5de0e71c95a63b
bc216857 <bc216857@falmouth.ac.uk>
2019-10-14 21:39:42 +0100

    ### ADDED * Follows the curve

--- Bezier.h
+++ Bezier.h
@@ -8,10 +8,9 @@ public:
 	Bezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3);
 
 	void draw(SDL_Renderer *renderer) const;
-
+	Vector2 CalculateBezier(float t);
 private:
 	const static int CURVEYSAMPLES = 20;
 	Vector2 p0, p1, p2, p3;
 	Vector2 incrementalPoints[CURVEYSAMPLES + 1];
-	Vector2 CalculateBezier(float t);
 };
