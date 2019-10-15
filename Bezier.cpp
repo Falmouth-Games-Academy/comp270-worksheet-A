44ef49609c5e33a20ac0bebdfd34e1407369735f
jakob <jc220866@falmouth.ac.uk>
2019-10-15 14:03:24 +0100

    added a couple lines, now we draw the shape but the lines are straight oh no

--- Bezier.cpp
+++ Bezier.cpp
@@ -10,5 +10,30 @@ void Bezier::draw(SDL_Renderer *renderer) const
 {
 	// TODO
 
-	SDL_RenderDrawLine( renderer, p0.x, p0.y, p1.x, p1.y );
+
+
+	for ( int i = 0; i < curveCount; i++ )
+	{
+		float t = i / curveCount;
+
+		
+		Vector2 point = pow( (1 - t), 3 ) * p0 + 3 * pow( (1 - t), 2 ) * t * p1 + 3 * (1 - t) * pow( t, 2 ) * p2 + pow( t, 3 ) * p3;
+
+		float tt = (i + 1) / curveCount;
+		Vector2 pointTwo_PointBoogaloo = pow( (1 - tt), 3 ) * p0 + 3 * pow( (1 - tt), 2 ) * tt * p1 + 3 * (1 - tt) * pow( tt, 2 ) * p2 + pow( tt, 3 ) * p3;
+
+		SDL_RenderDrawLine( renderer, point.x, point.y, pointTwo_PointBoogaloo.x, pointTwo_PointBoogaloo.y );
+
+
+	}
+
+
+
+
+
+
+
+
+
 }
+
--- Bezier.h
+++ Bezier.h
@@ -7,8 +7,14 @@ class Bezier
 public:
 	Bezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3);
 
+	
+
 	void draw(SDL_Renderer *renderer) const;
 
 private:
+
 	Vector2 p0, p1, p2, p3;
+
+	int curveCount = 20;
+
 };
