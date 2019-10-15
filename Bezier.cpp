547c979625ce83517ae2cb91438ebe153d753898
jakob <jc220866@falmouth.ac.uk>
2019-10-15 14:03:54 +0100

    added SDL convert vector to point but that didn't do anythign

--- Bezier.cpp
+++ Bezier.cpp
@@ -16,17 +16,18 @@ void Bezier::draw(SDL_Renderer *renderer) const
 	{
 		float t = i / curveCount;
 
-		
-		Vector2 point = pow( (1 - t), 3 ) * p0 + 3 * pow( (1 - t), 2 ) * t * p1 + 3 * (1 - t) * pow( t, 2 ) * p2 + pow( t, 3 ) * p3;
+
+		Vector2 voint = pow( (1 - t), 3 ) * p0 + 3 * pow( (1 - t), 2 ) * t * p1 + 3 * (1 - t) * pow( t, 2 ) * p2 + pow( t, 3 ) * p3;
+		SDL_Point point; point.x = voint.x; point.y = voint.y;
 
 		float tt = (i + 1) / curveCount;
-		Vector2 pointTwo_PointBoogaloo = pow( (1 - tt), 3 ) * p0 + 3 * pow( (1 - tt), 2 ) * tt * p1 + 3 * (1 - tt) * pow( tt, 2 ) * p2 + pow( tt, 3 ) * p3;
+		Vector2 vointTwo = pow( (1 - tt), 3 ) * p0 + 3 * pow( (1 - tt), 2 ) * tt * p1 + 3 * (1 - tt) * pow( tt, 2 ) * p2 + pow( tt, 3 ) * p3;
+		SDL_Point pointTwo; pointTwo.x = vointTwo.x; pointTwo.y = vointTwo.y;
 
-		SDL_RenderDrawLine( renderer, point.x, point.y, pointTwo_PointBoogaloo.x, pointTwo_PointBoogaloo.y );
+		SDL_RenderDrawLine( renderer, point.x, point.y, pointTwo.x, pointTwo.y );
 
 
 	}
-
 
 
 
